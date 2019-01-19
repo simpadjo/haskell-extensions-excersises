fa71a89fd7028f7a5bcdc6743337f6601c131848
evgenii <evgenii@adsquare.com>
2019-01-19 20:43:29 +0100

    4, 5 (partial)

--- exercise04/src/Exercises.hs
+++ exercise04/src/Exercises.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DataKinds      #-}
 {-# LANGUAGE GADTs          #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeOperators #-}
 module Exercises where
 
-import Data.Kind (Type)
+import Data.Kind (Type, Constraint)
 import Data.Function ((&))
-
+import Data.Semigroup hiding (Sum, Product)
+import Prelude hiding ((!!))
 
 
 
@@ -21,7 +25,21 @@ data IntegerMonoid = Sum | Product
 -- | a. Write a newtype around 'Integer' that lets us choose which instance we
 -- want.
 
+newtype MyInt (mon :: IntegerMonoid) = MyInt { unwrap :: Integer }
+
 -- | b. Write the two monoid instances for 'Integer'.
+
+instance Semigroup (MyInt Sum) where
+  (<>) (MyInt a) (MyInt b) = MyInt (a + b)
+
+instance Monoid (MyInt Sum) where
+  mempty = MyInt 0
+
+instance Semigroup (MyInt Product) where
+  (<>) (MyInt a) (MyInt b) = MyInt (a * b)
+
+instance Monoid (MyInt Product) where
+  mempty = MyInt 1
 
 -- | c. Why do we need @FlexibleInstances@ to do this?
 
@@ -41,10 +59,16 @@ data Void -- No constructors!
 
 -- | b. What are the possible type-level values of kind 'Maybe Void'?
 
+class MaybeVoidKind (a :: Maybe Void)
+instance MaybeVoidKind Nothing
+
 -- | c. Considering 'Maybe Void', and similar examples of kinds such as
 -- 'Either Void Bool', why do you think 'Void' might be a useful kind?
 
+class EitherVoid (a :: Either Void Bool)
+instance EitherVoid ('Right True)
 
+--TODO:
 
 
 
@@ -56,12 +80,18 @@ data Void -- No constructors!
 
 data Nat = Z | S Nat
 
-data StringAndIntList (stringCount :: Nat) where
-  -- ...
+data StringAndIntList (stringCount :: Nat) (intCount :: Nat) where
+  SNil :: StringAndIntList Z Z
+  AndInt :: Int -> StringAndIntList n k -> StringAndIntList n (S k)
+  AndString :: String -> StringAndIntList n k -> StringAndIntList (S n) k
 
 -- | b. Update it to keep track of the count of strings /and/ integers.
 
 -- | c. What would be the type of the 'head' function?
+head' :: StringAndIntList n k -> Maybe (Either String Int)
+head' SNil = Nothing
+head' (AndInt i _) = Just $ Right i
+head' (AndString s _) = Just $ Left s
 
 
 
@@ -79,18 +109,29 @@ data Showable where
 -- stores this fact in the type-level.
 
 data MaybeShowable (isShowable :: Bool) where
-  -- ...
+  IndeedShowable :: Show a => a -> MaybeShowable True
+  NotShowable :: a -> MaybeShowable False
 
 -- | b. Write a 'Show' instance for 'MaybeShowable'. Your instance should not
 -- work unless the type is actually 'show'able.
+instance Show (MaybeShowable True) where
+  show (IndeedShowable a) = show a
 
 -- | c. What if we wanted to generalise this to @Constrainable@, such that it
 -- would work for any user-supplied constraint of kind 'Constraint'? How would
 -- the type change? What would the constructor look like? Try to build this
 -- type - GHC should tell you exactly which extension you're missing.
 
+data MaybeConstrained (c :: Type -> Constraint) (isConstrained :: Bool) where
+  IndeedConstrained :: (c a) => a -> MaybeConstrained c True
+  NotConstrained :: a -> MaybeConstrained c False
 
 
+stringShowable :: MaybeConstrained Show True
+stringShowable = IndeedConstrained "x"
+
+stringNotShowable :: MaybeConstrained Show False
+stringNotShowable = NotConstrained "y"
 
 
 {- FIVE -}
@@ -105,17 +146,19 @@ data List a = Nil | Cons a (List a)
 -- having a list of types!
 
 data HList (types :: List Type) where
-  -- HNil  :: ...
-  -- HCons :: ...
+  HNil  :: HList Nil
+  HCons :: a -> HList tl -> HList (Cons a tl)
 
 -- | b. Write a well-typed, 'Maybe'-less implementation for the 'tail' function
 -- on 'HList'.
 
+hhtail :: HList (Cons (x :: Type) (xs :: List Type)) -> HList xs
+hhtail (HCons _ t) = t
+
 -- | c. Could we write the 'take' function? What would its type be? What would
 -- get in our way?
 
-
-
+--we need type-level (++) operator and nength hints
 
 
 {- SIX -}
@@ -134,17 +177,35 @@ data BlogAction
 -- Remember that, by switching on @DataKinds@, we have access to a promoted
 -- version of 'Bool'!
 
+data BlAction (isSafe :: Bool) where
+  AddBl :: BlAction True
+  DeleteBl :: BlAction False
+  AddCm :: BlAction True
+  DeleteCm :: BlAction False
+
 -- | b. Write a 'BlogAction' list type that requires all its members to be
 -- the same "access level": "admin" or "non-admin".
 
--- data BlogActionList (isSafe :: ???) where
---   ...
+data BlogActionList (isSafe :: Bool) where
+  BlNil :: BlogActionList c
+  BlCons :: (BlAction c) -> (BlogActionList c) -> BlogActionList c
 
 -- | c. Let's imagine that our requirements change, and 'DeleteComment' is now
 -- available to a third role: moderators. Could we use 'DataKinds' to introduce
 -- the three roles at the type-level, and modify our type to keep track of
 -- this?
+data Role = User | Admin | Moderator
 
+data BlogAction2 (r :: Role) where
+  AddBlogAsUser :: BlogAction2 User
+  AddBlogAsAdmin :: BlogAction2 Admin
+  AddBlogAsModerator :: BlogAction2 Moderator
+  RemoveBlog :: BlogAction2 Admin
+  AddCommentAsUser :: BlogAction2 User
+  AddCommentAsAdmin :: BlogAction2 Admin
+  AddCommentAsModerator :: BlogAction2 Moderator
+  DeleteCommentAsAdmin :: BlogAction2 Admin
+  DeleteCommentAsModerator :: BlogAction2 Moderator
 
 
 
@@ -166,16 +227,19 @@ data SBool (value :: Bool) where
 -- | a. Write a singleton type for natural numbers:
 
 data SNat (value :: Nat) where
-  -- ...
+  SZero :: SNat Z
+  SSucc :: SNat n -> SNat (S n)
 
 -- | b. Write a function that extracts a vector's length at the type level:
 
-length :: Vector n a -> SNat n
-length = error "Implement me!"
+len :: Vector n a -> SNat n
+len VNil = SZero
+len (VCons _ t) = SSucc (len t)
 
 -- | c. Is 'Proxy' a singleton type?
 
 data Proxy a = Proxy
+--yes
 
 
 
@@ -187,21 +251,21 @@ data Proxy a = Proxy
 -- and write to a file. To do this, we might write a data type to express our
 -- intentions:
 
-data Program                     result
-  = OpenFile            (Program result)
-  | WriteFile  String   (Program result)
-  | ReadFile  (String -> Program result)
-  | CloseFile (          Program result)
-  | Exit                         result
+--data Program                     result
+--  = OpenFile            (Program result)
+--  | WriteFile  String   (Program result)
+--  | ReadFile  (String -> Program result)
+--  | CloseFile (          Program result)
+--  | Exit                         result
 
 -- | We could then write a program like this to use our language:
 
-myApp :: Program Bool
-myApp
-  = OpenFile $ WriteFile "HEY" $ (ReadFile $ \contents ->
-      if contents == "WHAT"
-        then WriteFile "... bug?" $ Exit False
-        else CloseFile            $ Exit True)
+--myApp :: Program Bool
+--myApp
+--  = OpenFile $ WriteFile "HEY" $ (ReadFile $ \contents ->
+---      if contents == "WHAT"
+--        then WriteFile "... bug?" $ Exit False
+--        else CloseFile            $ Exit True)
 
 -- | ... but wait, there's a bug! If the contents of the file equal "WHAT", we
 -- forget to close the file! Ideally, we would like the compiler to help us: we
@@ -218,6 +282,19 @@ myApp
 -- type of 'myApp' that, once the program has completed, the file will be
 -- closed.
 
+data FileState = Closed | Reading | Writing
+
+--TODO:
+--data Program (s :: FileState) result where
+--  Initial :: Program Closed
+--  OpenR :: (Program Closed result) -> Program Reading result
+--  OpenW :: (Program Closed result) -> Program Writing result
+--  WriteFile :: (Program Writing result) -> String -> (Program Writing result)
+--  ReadFile ::  (Program Reading result) -> (String -> Program Reading result)
+--  CloseR :: (Program Reading result) -> (Program Closed result)
+--  CloseW :: (Program Writing result) -> (Program Closed result)
+--  Exit :: (Program Closed result) -> result
+
 -- | Improve the 'Program' type to keep track of whether a file is open.  Make
 -- sure the constructors respect this flag: we shouldn't be able to read or
 -- write to the file unless it's open. This exercise is a bit brain-bending;
@@ -226,8 +303,8 @@ myApp
 -- | EXTRA: write an interpreter for this program. Nothing to do with data
 -- kinds, but a nice little problem.
 
-interpret :: Program {- ??? -} a -> IO a
-interpret = error "Implement me?"
+--interpret :: Program {- ??? -} a -> IO a
+--interpret = error "Implement me?"
 
 
 
@@ -249,11 +326,17 @@ data Vector (n :: Nat) (a :: Type) where
 -- into Z and S cases. That's all the hint you need :)
 
 data SmallerThan (limit :: Nat) where
+  Ze :: (SmallerThan (S n))
+  Plus1 :: SmallerThan n -> SmallerThan (S n)
   -- ...
 
 -- | b. Write the '(!!)' function:
 
 (!!) :: Vector n a -> SmallerThan n -> a
-(!!) = error "Implement me!"
+--(!!) VNil _ = impossible TODO: how to cover?
+(!!) (VCons h _) Ze = h
+(!!) (VCons _ tl) (Plus1 k) = (!!) tl k
 
--- | c. Write a function that converts a @SmallerThan n@ into a 'Nat'.
+toInt :: SmallerThan n -> Int
+toInt Ze = 0
+toInt (Plus1 x) = (toInt x) +1
--- exercise05/src/Exercises.hs
+++ exercise05/src/Exercises.hs
@@ -20,11 +20,14 @@ data Exlistential where
 
 -- | a. Write a function to "unpack" this exlistential into a list.
 
--- unpackExlistential :: Exlistential -> (forall a. a -> r) -> [r]
--- unpackExlistential = error "Implement me!"
+unpackExlistential :: Exlistential -> (forall a. a -> r) -> [r]
+unpackExlistential Nil _ = []
+unpackExlistential (Cons x xs) f = (f x) : (unpackExlistential xs f)
 
 -- | b. Regardless of which type @r@ actually is, what can we say about the
 -- values in the resulting list?
+
+--could be only Unit
 
 -- | c. How do we "get back" knowledge about what's in the list? Can we?
 
@@ -42,15 +45,17 @@ data CanFold a where
 
 -- | a. The following function unpacks a 'CanFold'. What is its type?
 
--- unpackCanFold :: ???
--- unpackCanFold f (CanFold x) = f x
+unpackCanFold :: (forall f . Foldable f => f a -> r) -> CanFold a -> r
+unpackCanFold f (CanFold x) = f x
 
 -- | b. Can we use 'unpackCanFold' to figure out if a 'CanFold' is "empty"?
 -- Could we write @length :: CanFold a -> Int@? If so, write it!
-
+len :: CanFold a -> Int
+len c = unpackCanFold length c
 -- | c. Write a 'Foldable' instance for 'CanFold'. Don't overthink it.
 
-
+instance Foldable CanFold where
+  foldr f v x = unpackCanFold (foldr f v) x
 
 
 
@@ -64,14 +69,20 @@ data EqPair where
 -- | a. Write a function that "unpacks" an 'EqPair' by applying a user-supplied
 -- function to its pair of values in the existential type.
 
+unpackPair :: (forall a . Eq a => a -> a -> r) -> EqPair -> r
+unpackPair f (EqPair x y) = f x y
 -- | b. Write a function that takes a list of 'EqPair's and filters it
 -- according to some predicate on the unpacked values.
+
+pairFilter :: (forall a . Eq a => a -> a -> Bool) -> [EqPair] -> [EqPair]
+pairFilter f l = filter (unpackPair f) l
 
 -- | c. Write a function that unpacks /two/ 'EqPair's. Now that both our
 -- variables are in rank-2 position, can we compare values from different
 -- pairs?
 
-
+unpack2 :: (forall a . Eq a => a -> a -> r) -> EqPair -> EqPair -> (r ,r)
+unpack2 f p1 p2 = (unpackPair f p1, unpackPair f p2)
 
 
 
@@ -97,10 +108,13 @@ data Nested input output subinput suboutput
 -- | a. Write a GADT to existentialise @subinput@ and @suboutput@.
 
 data NestedX input output where
-  -- ...
+  Single :: Component input output -> NestedX input output
+  Combine :: Nested i o si so -> NestedX i o
 
 -- | b. Write a function to "unpack" a NestedX. The user is going to have to
 -- deal with all possible @subinput@ and @suboutput@ types.
+
+--TODO:
 
 -- | c. Why might we want to existentialise the subtypes away? What do we lose
 -- by doing so? What do we gain?
@@ -135,16 +149,21 @@ data Text = Text String
 class Renderable component where render :: component -> String
 
 -- | a. Write a type for the children.
-
+data RenderableEx where
+  MkRenderable :: Renderable c => c -> RenderableEx
+data HTML = HTML { properties :: (String, String), children :: [RenderableEx] }
 -- | b. What I'd really like to do when rendering is 'fmap' over the children
 -- with 'render'; what's stopping me? Fix it!
-
+renderHTML :: HTML -> String
+renderHTML (HTML (a, b) children) = concat $ fmap f children where
+                                       f :: RenderableEx -> String
+                                       f (MkRenderable c) = render c
 -- | c. Now that we're an established Haskell shop, we would /also/ like the
 -- option to render our HTML to a Shakespeare template to write to a file
 -- (http://hackage.haskell.org/package/shakespeare). How could we support this
 -- new requirement with minimal code changes?
 
-
+--TODO:
 
 
 
@@ -160,7 +179,7 @@ data MysteryBox a where
 
 -- | a. Knowing what we now know about RankNTypes, we can write an 'unwrap'
 -- function! Write the function, and don't be too upset if we need a 'Maybe'.
-
+--TODO
 -- | b. Why do we need a 'Maybe'? What can we still not know?
 
 -- | c. Write a function that uses 'unwrap' to print the name of the next
@@ -183,7 +202,8 @@ data SNat (n :: Nat) where
 -- | We also saw that we could convert from an 'SNat' to a 'Nat':
 
 toNat :: SNat n -> Nat
-toNat = error "You should already know this one ;)"
+toNat SZ = Z
+toNat (SS n) = S (toNat n)
 
 -- | How do we go the other way, though? How do we turn a 'Nat' into an 'SNat'?
 -- In the general case, this is impossible: the 'Nat' could be calculated from
@@ -195,6 +215,18 @@ toNat = error "You should already know this one ;)"
 -- SNat-accepting function (maybe at a higher rank?) that returns an @r@, and
 -- then returns an @r@. The successor case is a bit weird here - type holes
 -- will help you!
+
+--TODO:
+fromNat :: Nat -> (forall n . SNat n -> r) -> r
+fromNat Z f = f (SZ)
+--fromNat (S n) f = f (fromNat n g)
+
+--data SomeNat where
+--  MkSomeNat :: (SNat n) -> SomeNat
+--toExistential :: Nat -> SomeNat
+--toExistential Z = MkSomeNat SZ
+--toExistential (S k) = let (MkSomeNat x) = toExistential k in
+--                      MkSomeNat (SS x)
 
 -- | If you're looking for a property that you could use to test your function,
 -- remember that @fromNat x toNat === x@!
@@ -214,3 +246,12 @@ data Vector (n :: Nat) (a :: Type) where
 -- | It would be nice to have a 'filter' function for vectors, but there's a
 -- problem: we don't know at compile time what the new length of our vector
 -- will be... but has that ever stopped us? Make it so!
+data SomeVect a where
+  SomeVect :: Vector n a -> SomeVect a
+
+filterV :: (a -> Bool) -> Vector n a -> SomeVect a
+filterV _ VNil = SomeVect VNil
+filterV f (VCons x xs) =  let tl = filterV f xs in
+                          if (f x) then (append' x tl) else  tl where
+                            append' :: a -> SomeVect a -> SomeVect a
+                            append' y (SomeVect v) = SomeVect (VCons y v)
